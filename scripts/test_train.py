da1ff6d3214b0a8ed449309d2de1fe5c9f700a05
Naphat <naphat.mars@gmail.com>
2025-07-30 16:33:17 +0700

    test: update test_train.py

--- scripts/test_train.py
+++ scripts/test_train.py
@@ -777,7 +777,8 @@ def train_model(model_args, data_args, training_args):
             os.path.dirname(os.path.dirname(data_args.generations_filepath)),
             "eval_results_lm.txt",
         )
-    if data_args.generations_filepath or trainer.is_world_process_zero():
+    # if data_args.generations_filepath or trainer.is_world_process_zero():
+    if True:
         with open(output_eval_file, "a+") as writer:
             for key in results.keys():
                 if results[key] is not None:
@@ -811,10 +812,10 @@ if __name__ == "__main__":
     n_shots = 16
     explanation_sep = " because "
     project = "T5_Large_All"
-    exp_name = "Flan_T5_Large_Decoder"
+    exp_name = "Lora POC"
     for seed in seeds:
         run_name = f"{task}-{seed}-{model_name}-1-300-2-0-3e-05-2-30-350-{explanation_sep.strip()}-{model_name.replace('/','')}{io_format}-{n_shots}".replace(" ", "")
-        output_dir = os.path.join("exp_name", run_name)
+        output_dir = os.path.join(exp_name, run_name)
         os.makedirs(output_dir, exist_ok=True)
 
         os.environ["WANDB_PROJECT"] = project
